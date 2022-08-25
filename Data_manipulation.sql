e1cb0c41ffd0c52d9813e8c4ed5730ef36277a98
Filip Ivanusec <fivanusec@tria.hr>
2022-08-25 09:52:36 +0200

    Commit

--- Data_manipulation.sql
+++ Data_manipulation.sql
@@ -1,9 +1,29 @@
-select 
-    count(*) as c,
-    d.gradska_cetvrt
-from 
-    dataset d 
-GROUP BY
-    d.gradska_cetvrt
-order BY 
-    d.gradska_cetvrt
+-- select 
+--     count(*) as c
+-- from 
+--     dataset d 
+-- GROUP BY
+--     d.gradska_cetvrt,
+--     d.najmoprimac
+-- ORDER BY
+--     c asc
+-- ;
+
+-- select
+--     count(*) as c,
+--     d.najmoprimac
+-- from
+--     dataset d
+-- group by 
+--     d.najmoprimac
+-- having
+--     count(d.najmoprimac) > 1
+-- order by
+--     c DESC
+-- ;
+
+SELECT
+    d."Tijelo_samouprave",
+    d."Iznos"
+from mytable d
+order by d."Glava" desc;
